73f791eef86a13f21f4aa9e84842edf9220d6c75
Abdelhak <79027870+abdelhakamami@users.noreply.github.com>
2023-03-29 01:35:55 +0100

    Update admin-register-service.js

--- front-end/src/pages/admin-register/services/admin-register-service.js
+++ front-end/src/pages/admin-register/services/admin-register-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const adminRegister = async (values) => {
-  const response = await axios.post(`${process.env.REACT_APP_BACKEND_API}/admins`, values);
+  const response = await axios.post(`${process.env.REACT_APP_BACKEND_API}/api/admins`, values);
   return response.data;
 };
